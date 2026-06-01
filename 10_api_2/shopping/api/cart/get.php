6a1308dcf6343e0c7ea32c26dd7c9d09ce44e2ba
yse-yoo <youhei_yoshikawa@yse-c.net>
2026-06-01 14:40:03 +0900

    update

--- 10_api_2/shopping/api/cart/get.php
+++ 10_api_2/shopping/api/cart/get.php
@@ -1,7 +1,9 @@
 <?php
+// セッションの開始
 session_start();
+// JSON形式でレスポンスを返すためのヘッダー
 header('Content-Type: application/json');
-
+// JSONデータをレスポンスする
 echo json_encode([
     'status' => 'success',
     'cart' => $_SESSION['cart'] ?? [],
